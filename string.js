b84a636f52bd380e10a774c2b3792d511cdc020a
kr-pankil <pankil.t@knackroot.com>
2021-12-03 19:05:47 +0530

    string practice

---
+++ string.js
@@ -0,0 +1,14 @@
+ var firstString ="pankil"; 
+ var firstLength = 0;
+ firstLength = "pankil".length;
+ console.log(firstLength);
+ var secondString="tilvani";
+ firstLength=secondString.length;
+ console.log(firstLength);
+
+ var firstLetter = "";
+ firstLetter= firstString[0];
+ console.log(firstLetter);
+ 
+ var lastChar = firstString[firstString.length -1];
+ console.log(lastChar);
