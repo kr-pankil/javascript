ea17b4ec0aec72c927cf66ea271acd2fd48862ea
kr-pankil <pankil.t@knackroot.com>
2021-12-03 19:04:27 +0530

    array practice

---
+++ array.js
@@ -0,0 +1,15 @@
+/*var array= [["pankil",1],["tilvani",2]];
+console.log(array);
+var secondArray=[1,6,2,5];
+secondArray[1]=1;
+console.log(secondArray);
+var myArray=[[1,2,3],[4,5,6],[6,8,9],[[10,11,12],13,14]];
+var myData=myArray[3][0][1];
+console.log(myData);
+myArray.push([15,16]);
+console.log(myArray); */
+var myArray=[1,2,3];
+myArray.push( 4,5,6);
+console.log(myArray);
+myArray.unshift(-2,-1,0);
+console.log(myArray);
