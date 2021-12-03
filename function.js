d9779a2add393f4ca4115474fc0ff3cbfc1590f5
kr-pankil <pankil.t@knackroot.com>
2021-12-03 19:06:19 +0530

    not completed function practice

---
+++ function.js
@@ -0,0 +1,57 @@
+/*function firstFunction() {
+    console.log("hello world!");
+}
+firstFunction();
+function sub(a,b)
+{
+    console.log(a-b);
+}
+sub(9,4);
+function sum(a,b)
+{
+    console.log(a+b);
+}
+sum(9,4);
+//global var
+function fun(){
+var myGlobal =5;
+}
+function f1()
+{
+    console.log(myGlobal+5);
+}
+function f2()
+{
+    console.log(myGlobal-5);
+}
+f1();f2();
+var sum=0;
+function addFive()
+{
+    console.log(sum+=5);
+    
+}
+addFive();
+function demo(arr,item)
+{   
+    arr.push(item);
+    return arr.shift();
+}
+var testarr=[1,2,3,4,5];
+console.log("Before:"+ testarr);
+console.log(demo(testarr,6));
+console.log("After",testarr);
+
+var lol=4;
+if(lol=='4')
+{
+    console.log("laugh");
+}
+if (lol ==='4'){//'4' string so string equality (===) false
+    console.log("go & cry");
+}*/
+var lol='4';
+if(lol!=='4')
+{
+    return 4;
+}
